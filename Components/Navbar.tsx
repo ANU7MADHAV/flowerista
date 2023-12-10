cf979c3e1a140c21a0c68437c71538c61a77f6d4
ANU7MADHAV <anumadhavan888@gmail.com>
2023-12-10 21:59:14 +0530

    navbar added

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -2,17 +2,22 @@ import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 const Navbar = () => {
+  const links = [
+    { label: "Home", value: "/" },
+    { label: "Explore", value: "/explore" },
+    { label: "About", value: "/about" },
+  ];
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>Hey</li>
-          <li>
-            <Link href="/sign-in">Sign in</Link>
-            <UserButton />
-          </li>
+    <div className="border-b-4 shadow-md">
+      <nav className=" flex justify-between px-6 py-3">
+        <ul className="flex space-x-5">
+          {links.map((link) => (
+            <li key={link.label}>
+              <Link href={link.value}>{link.label}</Link>
+            </li>
+          ))}
         </ul>
+        <UserButton />
       </nav>
     </div>
   );
